65717a44e2e59bd079fa61d3c9ce5d9f2175a642
ckesav <keshav.pheelkhana@gmail.com>
2024-02-20 13:39:56 +0530

    add forms

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -1,26 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClient} from '@angular/common/http';
-import {CommonModule} from '@angular/common';
+import {ReservationService} from './reservation.service';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule,CommonModule,],
+  imports: [RouterOutlet, HttpClientModule, CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'reservation-app';
-  constructor(private http:HttpClient) {}
-  private baseUrl:String ='http://localhost:8080';
-  private reservationUrl:String = this.baseUrl+'/room/v1/reservation';
-
-  rooms:any[]=[]
+  constructor( private http:HttpClient,
+    private currentCheckInValue: String, private currentCheckOutValue:String, private currentPrice:Number,
+    private currentRoomNumber:Number){}
+//   constructor(private http:HttpClient,){}
+  rooms:any[]=[];
+  roomSearchForm:FormGroup = new FormGroup({
+      checkin: new FormControl<string>(''),
+      checkout: new FormControl<string>(''),
+      roomNumber: new FormControl<string>('')
+      });
+//     rooms:Room[]
   ngOnInit(){
+
+//     this.roomSearchForm.valueChanges.subscribe(form => {
+//       this.currentCheckInValue = form.checkin;
+//       this.currentCheckOutValue = form.checkout;
+//       let roomValues : string[] = form.roomNumber.split('|');
+//       this.currentRoomNumber = Number(roomValues[0]);
+//       this.currentPrice = Number(roomValues[1]);
+//     })
+
   this.rooms = [
 //   {id:"123",roomNumber:"127",price:"150"},{id:"123",roomNumber:"127",price:"150"}
 //     this.rooms.push(new Room("wd","sw","qsd"))
--- reservation-app/src/app/reservation.service.ts
+++ reservation-app/src/app/reservation.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ReservationService {
 
-  constructor() { }
+    constructor() {}
+    private baseUrl:String ='http://localhost:8080';
+    private reservationUrl:String = this.baseUrl+'/room/v1/reservation';
 }
